b8f4ef5867b757bb466bf3402e85bb8663109813
Guilherme Carvalho Chehab <guilherme.chehab@gmail.com>
2021-07-09 19:05:41 -0300

    Add status led blinking for Arduino Leonardo

--- Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
+++ Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
@@ -17,14 +17,38 @@ Resistor https://amzn.to/2EhIbsf
 
 // Initiate IRremote on Pin 3.
 IRsend irsend;
+//int RXLED = 17;  // The RX LED has a defined Arduino pin
+int i=0;
 
 void setup()
 {
   irsend.enableIROut(38);//Lib function
-}
+  // pinMode(RXLED, OUTPUT);  // Set RX LED as an output
+  // TX LED is set as an output behind the scenes
+
+  Serial.begin(9600); //This pipes to the serial monitor
+  Serial.println("Initialize Serial Monitor");
+
+  Serial1.begin(9600); //This is the UART, pipes to sensors attached to board
+  Serial1.println("Initialize Serial Hardware UART Pins");}
 
 void loop()
 {
-  irsend.mark(1000);
-  irsend.space(1000);
+  //digitalWrite(RXLED, LOW);   // set the RX LED ON
+  TXLED0; //TX LED is not tied to a normally controlled pin so a macro is needed, turn LED OFF
+  delay (1000);
+
+  for (i=0; i<8; i++) {
+    irsend.mark(1000);
+    irsend.space(1000);
+  }
+  
+  //digitalWrite(RXLED, HIGH);    // set the RX LED OFF
+  TXLED1; //TX LED macro to turn LED ON
+  delay (1000);
+
+  for (i=0; i<8; i++) {
+    irsend.mark(1000);
+    irsend.space(1000);
+  }
 }
