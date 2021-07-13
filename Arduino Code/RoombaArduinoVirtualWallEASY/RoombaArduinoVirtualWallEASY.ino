4b35f95809fc8eb7cfe47c9b443dc78be821c4a7
Guilherme Carvalho Chehab <guilherme.chehab@gmail.com>
2021-07-13 15:30:52 -0300

    Added Roomba codes and loop now sends virtual code. Changed IR TX pin to pin3 -- for arduino pro micro

--- Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
+++ Arduino Code/RoombaArduinoVirtualWallEASY/RoombaArduinoVirtualWallEASY.ino
@@ -14,15 +14,15 @@ Resistor https://amzn.to/2EhIbsf
 */
 
 #include <IRremote.h>
+void roomba_send(int code); 
 
 // Initiate IRremote on Pin 3.
 IRsend irsend;
-//int RXLED = 17;  // The RX LED has a defined Arduino pin
+int RXLED = 17;  // The RX LED has a defined Arduino pin
 int i=0;
 
 void setup()
 {
-  irsend.enableIROut(38);//Lib function
   // pinMode(RXLED, OUTPUT);  // Set RX LED as an output
   // TX LED is set as an output behind the scenes
 
@@ -30,7 +30,10 @@ void setup()
   Serial.println("Initialize Serial Monitor");
 
   Serial1.begin(9600); //This is the UART, pipes to sensors attached to board
-  Serial1.println("Initialize Serial Hardware UART Pins");}
+  Serial1.println("Initialize Serial Hardware UART Pins");
+  irsend.enableIROut(38);//Lib function
+  irsend.setSendPin(3);
+}
 
 void loop()
 {
@@ -38,17 +41,53 @@ void loop()
   TXLED0; //TX LED is not tied to a normally controlled pin so a macro is needed, turn LED OFF
   delay (1000);
 
-  for (i=0; i<8; i++) {
-    irsend.mark(1000);
-    irsend.space(1000);
-  }
+  roomba_send (162);
   
   //digitalWrite(RXLED, HIGH);    // set the RX LED OFF
   TXLED1; //TX LED macro to turn LED ON
   delay (1000);
 
-  for (i=0; i<8; i++) {
-    irsend.mark(1000);
-    irsend.space(1000);
+  roomba_send (162);
+}
+
+void roomba_send(int code) 
+{
+  Serial.print("Sending Roomba code ");
+  Serial.print(code);
+  int length = 8;
+  unsigned int raw[length*2];
+  unsigned int one_pulse = 3000;
+  unsigned int one_break = 1000;
+  unsigned int zero_pulse = one_break;
+  unsigned int zero_break = one_pulse;
+
+  int arrayposition = 0;
+  // Serial.println("");
+  for (int counter = length-1; counter >= 0; --counter) {
+    if(code & (1<<counter)) {
+      // Serial.print("1");
+      raw[arrayposition] = one_pulse;
+      raw[arrayposition+1] = one_break;
+    } 
+    else {
+      // Serial.print("0");
+      raw[arrayposition] = zero_pulse;
+      raw[arrayposition+1] = zero_break;
+    }
+    arrayposition = arrayposition + 2;
+  } 
+  for (int i = 0; i < 3; i++) {
+    irsend.sendRaw(raw, 15, 38);
+    delay(50);
   }
+  Serial.println("");
+
+  /*
+  Serial.print("Raw timings:");
+   for (int z=0; z<length*2; z++) {
+   Serial.print(" ");
+   Serial.print(raw[z]);
+   }
+   Serial.print("\n\n");
+   */
 }
